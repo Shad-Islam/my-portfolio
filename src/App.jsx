eb5659fcd0ec211487758a58189e78a3e7fb422c
Shad-islam <m.shadislam01@gmail.com>
2025-04-09 16:48:26 +0600

    Add loading screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
 
 function App() {
-  return <></>;
+  const [loading, setLoading] = useState(false);
+  return (
+    <>
+ { !loading && <LoadingScreen onComplete={()=>setLoading(true)}/>}
+    </>
+  );
 }
 
 export default App;
